27665ffdfba0445a0b44dbe8a413a432ce20d56a
Andre Buhain <abuhain29@setoncatholic.org>
2026-09-23 18:14:14 +0000

    Calc complete

---
+++ main.py
@@ -0,0 +1,19 @@
+#function that adds two numbers
+def add(x,y):
+    print(x+y)
+#function that subtracts two numbers
+def subtract(x,y):
+    print(x-y)
+#function that multiplies two numbers
+def multiply(x,y):
+    print(x*y)
+#function that divideds two numbers
+def divide(x,y):
+    print(x/y)
+x = int(input("Enter the First Number: "))
+y = int(input("Enter the Second Number: "))
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+divide(x,y)
+
