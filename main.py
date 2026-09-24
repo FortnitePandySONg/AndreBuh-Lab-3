fa629c474019c3c96acde69525b9002f09dbfa92
Andre Buhain <abuhain29@setoncatholic.org>
2026-09-24 21:12:59 +0000

    calc complete

--- main.py
+++ main.py
@@ -16,4 +16,29 @@ add(x,y)
 subtract(x,y)
 multiply(x,y)
 divide(x,y)
+#Start of the Program
+print("Welcome to calc")
+print("What do you want to do")
+print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+calc_choice = input(": ")
+print(calc_choice)
+x = int(input("Enter the First Number: "))
+y = int(input("Enter the Second Number: "))
+while(1): 
+    if calc_choice == 'a':
+        add(x,y)
+    elif calc_choice == 's':
+        subtract(x,y)
+    elif calc_choice == 'm':
+        multiply(x,y)
+    elif calc_choice == 'd': 
+        divide(x,y)
+    elif calc_choice == 'q':
+        print("Bye!")
+        break
+    else:
+        print("Invalid input")
+        
+        
+
 
